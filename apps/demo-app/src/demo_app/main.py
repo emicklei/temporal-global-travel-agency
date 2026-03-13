a2b801d59c404dbb37204d6eadda51dd67ff7c8c
emicklei <ernest.micklei@gmail.com>
2026-03-13 16:44:57 +0100

    chore: remove demo-app completely

--- apps/demo-app/src/demo_app/main.py
+++
@@ -1,6 +0,0 @@
-def main() -> None:
-    print("demo")
-
-
-if __name__ == "__main__":
-    main()
--- apps/demo-app/tests/test_main.py
+++
@@ -1,7 +0,0 @@
-from demo_app.main import main
-
-def test_main(capsys) -> None:
-    main()
-    captured = capsys.readouterr()
-    assert captured.out.strip() == "demo"
-
